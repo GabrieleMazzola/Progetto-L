712092d9078b8b89bf244d040317dbeb1b0cd18a
GabrieleMazzola <gabriele.m1995@gmail.com>
2017-05-03 17:14:30 +0200

    Aggiunto DatabaseADAPTER

---
+++ TicketProject/src/databaseadapter/DatabaseAdapter.java
@@ -0,0 +1,98 @@
+
+package databaseadapter;
+
+import java.util.*;
+
+/**
+ *
+ * @author Gabriele
+ */
+public class DatabaseAdapter {
+    
+    private Set<UserDB> users;
+    private Set<TicketDB> tickets;
+    private Set<FineDB> fines;      //Verrà eliminata una volta che Simo invierà la sua parte del codice
+
+    public DatabaseAdapter() {
+        this.tickets = new HashSet<>();
+        this.users = new HashSet<>();
+        addUser(new UserDB("ADMIN", "ADMIN", "ADMIN"));
+        this.fines = new HashSet<>();
+    }
+
+    public boolean addUser(UserDB user) {
+        return users.add(user);
+    }
+    
+    public boolean addTicket(TicketDB ticket){
+        return tickets.add(ticket);
+    }
+    
+    private TicketDB getTicketByCode(String ticketCode){
+        for(TicketDB t: tickets){
+            if(t.getCode().trim().equalsIgnoreCase(ticketCode.trim())){
+                return t;
+            }
+        }
+        return null;
+    }
+    
+    public boolean existsTicket(String ticketCode){
+        return(getTicketByCode(ticketCode) != null);
+    }
+    
+    
+    public boolean isValid(String ticketCode){
+        TicketDB t = getTicketByCode(ticketCode);
+        if(t != null){
+            return t.isActive();
+        }
+        return false;
+    }
+    
+    public boolean checkUser(String cf){
+        for(UserDB u : users){
+            if(u.getCF().trim().equalsIgnoreCase(cf.trim())) return true;
+        }
+        return false;
+    }
+ 
+    public boolean activateTicket(String ticketCode){
+        TicketDB t;
+        if((t = getTicketByCode(ticketCode)) != null){
+            t.activate();
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean addFine(FineDB fine) {
+        return fines.add(fine);
+    }
+    
+    public Set<FineDB> getFineByCFCode(String cfCode) {
+        Set<FineDB> fittingFines = new HashSet<>();
+        
+        for(FineDB fine : fines) {
+            if(fine.getCfCode().equals(cfCode)) fittingFines.add(fine);
+        }
+        
+        return fittingFines;
+    }
+    
+    public void printUsers(){
+        System.out.println("\nUSERS:");
+        for(UserDB u : users){
+            System.out.println(u.toString());
+        }
+    }
+    
+    public void printTickets(){
+        System.out.println("\nTICKETS:");
+        for(TicketDB t : tickets){
+            System.out.println(t.toString());
+        }
+    }
+    
+    
+}
---
+++ TicketProject/src/databaseadapter/Testing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseadapter;
+
+/**
+ *
+ * @author Gabriele
+ */
+public class Testing {
+    
+    public static void main(String[] args) {
+        DatabaseAdapter mongoDB = new DatabaseAdapter();
+        mongoDB.addUser(new UserDB("Simone", "Colosi", "Millisecondi"));
+        mongoDB.addTicket(new TicketDB(TicketType.SINGLE));
+        mongoDB.addTicket(new TicketDB(TicketType.SINGLE));
+        mongoDB.activateTicket("0");
+        System.out.println(mongoDB.checkUser("Millisecondi"));  //true
+        System.out.println(mongoDB.checkUser("kajbsc"));        //false
+        System.out.println(mongoDB.existsTicket("0"));          //true
+        System.out.println(mongoDB.existsTicket("1"));          //true
+        System.out.println(mongoDB.existsTicket("2"));          //false
+        System.out.println(mongoDB.isValid("0"));               //true
+        System.out.println(mongoDB.isValid("1"));               //false
+        
+        mongoDB.printTickets();
+        mongoDB.printUsers();
+        
+    }
+    
+}
