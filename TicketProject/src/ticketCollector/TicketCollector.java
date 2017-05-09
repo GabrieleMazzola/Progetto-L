72ba8e17868faa0742a129a7a31fea291f621be7
SColosi <colosisimone@gmail.com>
2017-05-09 15:38:31 +0200

    Add files via upload

--- TicketProject/src/ticketCollector/TicketCollector.java
+++ TicketProject/src/ticketCollector/TicketCollector.java
@@ -1,21 +1,43 @@
-package TicketCollector;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticketcollectors;
 
 import databaseadapter.DatabaseAdapter;
+import databaseadapter.TicketDB;
 import java.util.Date;
 
+/**
+ *
+ * @author Simone
+ */
 public class TicketCollector {
     
-    private DatabaseAdapter mongoDb;
+    private final DatabaseAdapter mongoDb;
     
-    public TicketCollector(){
-        mongoDb=new DatabaseAdapter();
+    public TicketCollector(DatabaseAdapter mongoDB){
+        this.mongoDb=mongoDB;
     }
+    
+    public boolean verifyValidation(String idTicket){
+        
+         
+         TicketDB t = mongoDb.getTicketByCode(idTicket);
+         if(t == null){
+             throw new FineException("Non esiste un biglietto con questo codice: " + idTicket);
+         }     
+         return t.isActive();
+         
+    }
+      
     
     public boolean fine(String cf,Date today){
     
         if(mongoDb.addFine(new Fine(cf,today)))
             return true;
-        throw new FineException("Non è stato possibile aggiungere la multa");
+        throw new FineException("Non è stato possibile aggiungere la multa: "+ cf);
         
     }
     
