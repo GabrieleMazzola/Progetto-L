9d9edd27d9bd1c0a7cbb2256465f3cea331d13b6
ImReds <areds13@gmail.com>
2017-05-10 15:48:59 +0200

    Finito algoritmo di Luhn

--- TicketProject/src/bank/CreditCardAlgorithm.java
+++ TicketProject/src/bank/CreditCardAlgorithm.java
@@ -10,41 +10,21 @@ package bank;
  * @author Andrea
  */
 public class CreditCardAlgorithm {
-    private static int[] codes;
     
-    public static boolean isValidNumber(String code) {
-        int[] codes = getCodeFromString(code);
-        return true;
-    }
-    
-    private static int getDoubledEvenDigitsSum() {
-        int[] cifre = doubleEvenDigits();
-        int tot = 0;
-        
-        for(int num : cifre) tot += num;
-        
-        return tot;
-    }
-    
-    private static int getOddDigitsSum() {
-        return 1;
-    }
-    
-    private static int[] doubleEvenDigits() {
-        int[] cifre = new int[codes.length/2];
-        
-        for(int i = 0; i < codes.length; i += 2) {
-            cifre[i] = codes[i]*2;
-            if(cifre[i] >= 10) cifre[i] -= 9; //Sommiamo le cifre pari moltiplicate per due (se la cifra i è maggiore di 
-                                              //10 vanno sommate le singole cifre)
-        }
-        
-        return cifre;
-    }
-    
-    private static int[] getCodeFromString(String code) {
-        String[] buffer = code.split(" ");
-        int[] codes = new int[16];
-        return codes;
+    public static boolean check(String ccNumber){
+            int sum = 0;
+            boolean alternate = false;
+            for (int i = ccNumber.length() - 1; i >= 0; i--){
+                    int n = Integer.parseInt(ccNumber.substring(i, i + 1));
+                    if (alternate){
+                            n *= 2;
+                            if (n > 9){
+                                    n = (n % 10) + 1;
+                            }
+                    }
+                    sum += n;
+                    alternate = !alternate;
+            }
+            return (sum % 10 == 0);
     }
 }
