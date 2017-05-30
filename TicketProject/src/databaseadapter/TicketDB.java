299121ed912b0173a5136fa51871d62b957686de
GabrieleMazzola <gabriele.m1995@gmail.com>
2017-05-30 15:42:05 +0200

    aggiunto singleton per codifica e decodifica data

--- TicketProject/src/databaseadapter/TicketDB.java
+++ TicketProject/src/databaseadapter/TicketDB.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package databaseadapter;
 
-/**
- *
- * @author Gabriele
- */
 public class TicketDB {
     private TicketType type;
     private String code;
