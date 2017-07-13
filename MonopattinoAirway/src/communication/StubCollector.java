86b4c80f4314d26d667d36e2d584e0e25380e198
Francesco <framagistri@gmail.com>
2017-07-13 17:32:29 +0200

    Aggoiunta Javadoc Skeleton,SocketHandler e StubCollector

--- MonopattinoAirway/src/communication/StubCollector.java
+++ MonopattinoAirway/src/communication/StubCollector.java
@@ -25,6 +25,14 @@ public class StubCollector implements CentralSystemCollectorInterface{
     private PrintWriter toServer;
     private List<Fine> offlineFines; 
     
+    /**
+     * Richiede ipAddress del CentralSystem(SocketHandler), e porta di comunicazione.
+     * Inizializza i relativi campi,le connessioni, i metodi di comunicazione, e la lista
+     * di multe salvate offline.
+     * @param ipAddress
+     * @param port
+     * @throws IOException 
+     */
     public StubCollector(String ipAddress,int port) throws IOException {
         this.ipAdress = ipAddress;
         this.port = port;
@@ -53,6 +61,16 @@ public class StubCollector implements CentralSystemCollectorInterface{
         }    
     }
     
+    /**
+     * Viene sempre fatto un test sull' integrità della connessione
+     * Attraverso il Singleton dei JSONOperation viene creato un pacchetto JSON che contiene il
+     * codice seriale da verificare, e questo viene spedito al SocketHandler del CentralSystem.
+     * Viene decodificata la risposta dal pacchetto JSON inviato in risposta dal SocketHandler.
+     * In caso di errore della comunicazione viene chiusa la connessione.
+     * @param serialcode
+     * @return Vero se il biglietto esiste, falso altrimenti e null se avviene un errore di connessione
+     * o di parsing del pacchetto JSON di risposta
+     */
     @Override
     public Boolean existsTicket(long serialcode) {
        
@@ -75,6 +93,16 @@ public class StubCollector implements CentralSystemCollectorInterface{
         return null;
     }
 
+    /**
+     * Comunica al SocketHandler tramite JSON una multa da immagazzinare nel database.
+     * In caso di interruzione della connessione, questa viene chiusa, e viene sempre
+     * fatto un test di integrità, e un tentativo di riconnessione all'inizio del metodo.
+     * Se non c'è connessione le multe vengono salvate offline, e poi rimandate 
+     * al ripristino della connessione.
+     * @param f
+     * @return Vero se si riesce/riescono ad inviare la/le Fine, falso in caso contrario, e null
+     * in caso di errore durante il parsing del pacchetto JSON
+     */
     @Override
     public Boolean addFine(Fine f){
         addOfflineFine(f);
@@ -120,6 +148,10 @@ public class StubCollector implements CentralSystemCollectorInterface{
         this.offlineFines.add(fine);
     }
     
+    /*Metodo privato che permette di comunicare al server le Fines salvate offline.
+     *Finchè la connessione è abilitata, e la lista delle fines offline non è vuota, viene creato
+     *un pacchetto JSON per ogni Fine nella lista, e questa viene comunicata al server.
+    */
     private Boolean saveFinesOnline() throws IOException, ParseException{
 
         while(!socket.isClosed() && !offlineFines.isEmpty()){
@@ -151,6 +183,13 @@ public class StubCollector implements CentralSystemCollectorInterface{
         }
     }
 
+    /**
+     * Richiede al SocketHandler, inviando un pacchetto JSON con l'username del Collector,
+     * di inviare il numero di multe fatte dallo specifico Collector fino a quel momento.
+     * @param collectorUsername
+     * @return Un long, che è il numero di multe fatte dal Collector, o null se avviene un problema
+     * di comunicazione o di parsing del pacchetto JSON di risposta
+     */
     public Long requestFinesStartNumber(String collectorUsername) {
         try {
             
