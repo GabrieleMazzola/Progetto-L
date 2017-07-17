e3e733e960038873d833fe8b66ef57a3c7e70cd0
zubeer95 <progetti.zubeer@gmail.com>
2017-07-17 14:52:53 +0200

    Aggiunti file mancati

---
+++ WebServer/ticket/src/main/java/centralsystem/CentralSystemWebServerInterface.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package centralsystem;
+
+import items.Fine;
+import items.Sale;
+import java.util.Set;
+
+/**
+ *
+ * @author Zubeer
+ */
+public interface CentralSystemWebServerInterface {
+
+
+    boolean existsTicket(Long ticketID);
+
+    boolean collectorLogin(String username, String password);
+
+    Sale getSale(String serialCode);
+
+    Boolean makeFine(Fine f);
+
+    Set<Sale> getSalesByUsername(String username);
+    public Set<Sale> getValidSalesByUsername(String username);
+    
+
+    String createUser(String name, String surname, String cf, String username, String password, String email);
+
+    Long countAllFinesMadeBy(String collectorUsername);
+    boolean userLogin(String username, String password);
+    
+}
